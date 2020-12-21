c39c2f6bdaf6ee02f2da90599aaf66218574b740
jonasviviani <jonasviviani2012@hotmail.com>
2020-12-21 16:25:10 -0300

    refactor: removido codigo duplicado para um metodo helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,21 +1,11 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../erros/missing-param-error'
-
+import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
-    }
+    if (!httpRequest.body.name || !httpRequest.body.email) { return badRequest(new MissingParamError('name')) }
 
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
-    }
+    if (!httpRequest.body.email) { return badRequest(new MissingParamError('email')) }
 
     return {
       statusCode: 400,

db5cdbc3c4f5fee95a60bc81fcc16af7770c0730
jonasviviani <jonasviviani2012@hotmail.com>
2020-12-21 16:40:19 -0300

    refactor: adicionado interface no arquivo SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,8 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
+import { Controller } from '../protocols/controller'
 import { MissingParamError } from '../erros/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 

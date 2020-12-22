001317826a41ff6bdb6a32cd164c78d4b21fc56d
jonasviviani <jonasviviani2012@hotmail.com>
2020-12-22 15:02:28 -0300

    feat: criado validador de email

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,23 @@
+import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
+
+describe('EmailValidator Adapter', () => {
+  test('Caso retorne falso, validador tambem retorna falso', () => {
+    const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+
+  test('Caso retorne true, validador tambem retorna true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
+  })
+})

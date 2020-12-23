48f2c42ff44d73fcaeb43fc1842daa6613db88ab
jonasviviani <jonasviviani2012@hotmail.com>
2020-12-23 11:27:38 -0300

    test: criado teste para verificar exception do Bcrypt

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,36 @@
+import { BcryptAdapter } from './bcrypt-adapter'
+import bcrypt from 'bcrypt'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hash'))
+  }
+}))
+
+const salt = 12
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  test('Chamar o bcrypt com os valores corretos', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Verificar se o hash esta correto', async () => {
+    const sut = makeSut()
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
+  })
+
+  test('Retornar exceção do Bcrypt', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
+})

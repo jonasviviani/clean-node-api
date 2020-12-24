db36c96c49ee78c93cd2c446b3fae530e9e530cc
jonasviviani <jonasviviani2012@hotmail.com>
2020-12-24 11:14:32 -0300

    feat: configurado mongoDb em memória da maquina e feito teste de integração

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,9 +1,13 @@
-module.exports = {
-  mongodbMemoryServerOptions: {
-    binary: {
-      version: '4.0.3',
-      skipMD5: true
+module.exports = { 
+  mongodbMemoryServerOptions: { 
+    binary: { 
+      version: '4.4.1', 
+      skipMD5: true 
+    }, 
+    instance: {
+      dbName: 'jest'
     },
-    autoStart: false
+    autoStart: false, 
+    instance: {} 
   }
 };
---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,35 @@
+import { MongoHelper } from '../helpers/mongo.helpers'
+import { AccountMongoRepository } from './account'
+
+describe('account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
+
+  test('Testar o retorno de sucesso do account', async () => {
+    const sut = makeSut()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email')
+    expect(account.password).toBe('any_password')
+  })
+})

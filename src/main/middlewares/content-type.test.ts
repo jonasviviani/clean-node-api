7cba7bcbbaf3fcd935dd254dfa19fbc5e1550549
jonasviviani <jonasviviani2012@hotmail.com>
2020-12-25 10:09:00 -0300

    feat: criado arquivos middlewares content-types

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content Type Middlewares', () => {
+  test('Testando content type retornar um json por default', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  test('Testando content type retornar um xml forçado', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})

80a2bf45e17f78875f2aae2aea5eabe68e4b80b7
Jonas Viviani <69729217+jonasviviani@users.noreply.github.com>
2020-12-28 13:57:06 -0300

    test: criado test de retorno do controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -46,4 +46,23 @@ describe('Log Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Testar retorno do controlador', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Jonas'
+      }
+    })
+  })
 })
